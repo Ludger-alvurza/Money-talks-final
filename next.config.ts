d23c394f367e6081eff7ff9e381d8b69f82e6f5a
ludgerdusdesi <lud.gerdus.p@mail.ukrim.ac.id>
2025-05-29 21:41:41 +0700

    PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+import withPWA from "@ducanh2912/next-pwa";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["example.com"], // Ganti dengan domain yang digunakan untuk gambar
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  fallbacks: {
+    document: "/offline.html",
+  },
+} as any)(nextConfig); // Type assertion to bypass strict typing
